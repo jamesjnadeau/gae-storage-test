f1675ad7faa83caa5fa3f77a57dcbcb08fe72d2b
jamesjnadeau <james.nadeau@gmail.com>
2014-09-29 16:28:33 -0400

    adding initial files

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+use google\appengine\api\cloud_storage\CloudStorageTools;
+$bucket = CloudStorageTools::getDefaultGoogleStorageBucketName();
+
+$file = "gs://".$bucket."/test.txt";
+
+file_put_contents($file, 'This is a test, please work' );
+sleep(2);
+echo file_get_contents($file);
